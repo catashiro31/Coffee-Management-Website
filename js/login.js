3cdca2cdcf2f12a1f46fd54e07e545c3bc737f45
Kast261 <kastgamingtv1604@gmail.com>
2025-10-24 14:50:19 +0700

    hoan thanh trang dang nhap dang ki

--- js/login.js
+++ js/login.js
@@ -9,3 +9,60 @@ registerBtn.addEventListener('click',() =>{
 loginBtn.addEventListener('click',() =>{
     container.classList.remove('active');
 });
+const socialIcons = document.querySelectorAll('.social-icons a');
+
+socialIcons.forEach(icon => {
+    icon.addEventListener('click', (event) => {
+        event.preventDefault();
+        alert('Tính năng này hiện chưa được cập nhật. Vui lòng Thử lại sau!');
+    });
+});
+
+const loginForm = document.querySelector('.form-box.login form');
+const loginUsernameInput = document.querySelector('input[type = "text"]');
+const loginPasswordInput = document.querySelector('input[type = "password"]');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    const username = loginUsernameInput.value;
+    const password = loginPasswordInput.value;
+
+    if (username === '' || password === '') {
+        alert('Vui lòng nhập đầy đủ Tên đăng nhập và Mật khẩu!');
+    } else {
+        alert('Đăng nhập thành công! (Đây là demo)');
+        loginForm.reset(); 
+    }
+});
+const registerForm = document.querySelector('.form-box.register form');
+const registerUsernameInput = registerForm.querySelector('input[type="text"]');
+const registerEmailInput = registerForm.querySelector('input[type="email"]');
+const registerPasswordInput = registerForm.querySelector('input[type="password"]');
+
+
+registerForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = registerUsernameInput.value;
+    const email = registerEmailInput.value;
+    const password = registerPasswordInput.value;
+    if (username === '' || email === '' || password === '') {
+        alert('Vui lòng điền đầy đủ thông tin đăng ký!');
+        return;
+    }
+    if (!isEmailValid(email)) {
+        alert('Địa chỉ Email không hợp lệ. Vui lòng kiểm tra lại.');
+        return;
+    }
+    if (password.length < 6) {
+        alert('Mật khẩu phải có ít nhất 6 ký tự.');
+        return;
+    }
+
+    alert('Đăng ký thành công! (Đây là demo)');
+    registerForm.reset();
+});
+
+function isEmailValid(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
